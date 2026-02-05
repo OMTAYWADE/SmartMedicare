ec67df4ecdd37dacb4459bb42f2ece1791f1bdab
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-02-05 21:03:00 +0530

    due to path missing

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ mongoose
 -------------------------------------------------- */
 
 // Set EJS as the view engine
+const path = require("path");
+
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
 
